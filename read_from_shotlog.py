8310e975ff902c711f0a4890496b5e988535fd0d
Jens von der Linden <jensvonderlinden@gmail.com>
2016-02-24 20:04:41 -0800

    Add functions to store in sql, read several shots.

--- read_from_shotlog.py
+++ read_from_shotlog.py
@@ -6,6 +6,15 @@ Created on Wed Jan 27 14:01:40 2016
 """
 
 import pandas as pd
+import sqlite3
+
+
+def read_and_store_shotlog(shots, database):
+    r"""
+    """
+    for shot in shots:
+        mach_settings = read_mach_settings_shotlog(shot)
+        store_mach_settings_sql(shot, mach_settings, database)
 
 
 def read_mach_settings_shotlog(shot):
@@ -13,7 +22,7 @@ def read_mach_settings_shotlog(shot):
     Return a dictionary with all mach probe settings from the shotlog for a
     specific shot number.
     """
-    path = '/Users/vonderlinden2/rsx_drive/RSX/MDS+/Shotlogs/shotlog_2013-05-02.5.xlsx'
+    path = '/Users/vonderlinden2/rsx_drive/RSX/MDS+/Shotlogs/shotlog_2013-05-02.8.xlsx'
     column = {'mach_insertion': 'Unnamed: 37',
               'mach_x': 'Unnamed: 38',
               'mach_x_read': 'Unnamed: 39',
@@ -28,3 +37,17 @@ def read_mach_settings_shotlog(shot):
     for key in column.keys():
         mach_settings[key] = shotlog[column[key]][shot]
     return mach_settings
+
+
+def store_mach_settings_sql(shot, mach_settings, database):
+    r"""
+    """
+    connection = sqlite3.connection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Shots SET mach_x = ?, mach_y = ?, mach_z = ?, " +
+                   "mach_orientation = ?  WHERE shot=?;",
+                   [mach_settings['mach_x'], mach_settings['mach_y'],
+                    mach_settings['mach_z'], shot])
+    connection.commit()
+    cursor.close()
+    connection.close()
