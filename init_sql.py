ffc773733df91dd51f02996d33217ff72c2e4c1c
Jens von der Linden <jensvonderlinden@gmail.com>
2016-03-02 11:40:06 -0800

    Add shot and mach quality columns.

--- init_sql.py
+++ init_sql.py
@@ -12,8 +12,12 @@ connection = sqlite3.connect('shots.db')
 cursor = connection.cursor()
 
 cursor.execute("CREATE TABLE Shots(shot INTEGER, existence BOOLEAN, " +
+               "campaign STRING, bias_current_on BOOLEAN, "+
+               "bias_current_pre_crowbar_regular BOOLEAN, "+
+               "fiducial_ocillates BOOLEAN, " +
                "zero_phase_time REAL, zero_phase_index INTEGER, " +
                "period FLOAT, ramp_time FLOAT, ramp_index INTEGER, " +
+               "mach_aquired BOOLEAN, mach_oscillates BOOLEAN, "
                "mach_x FLOAT, mach_y FLOAT, mach_z FLOAT, " +
                "mach_orientation FLOAT);")
 connection.commit()
