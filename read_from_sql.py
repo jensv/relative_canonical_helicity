aff33c5af2673480e29211f93d7ec1dcbe0fea34
Jens von der Linden <jensvonderlinden@gmail.com>
2016-10-19 18:39:59 -0700

    late commit. rename table_name to table

--- read_from_sql.py
+++ read_from_sql.py
@@ -7,28 +7,28 @@ Created on Wed Mar  2 09:49:01 2016
 
 import sqlite3
 
-def read_all_rows(condition, database, table_name):
+def read_all_rows(condition, database, table):
     r"""
     Return all rows from sql table that match condition.
     """
     connection = sqlite3.connect(database)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM ' + table_name + ' WHERE ' + condition)
+    cursor.execute('SELECT * FROM ' + table + ' WHERE ' + condition)
     rows = cursor.fetchall()
     cursor.close()
     connection.close()
     return rows
 
 
-def cursor_with_rows(condition, database, table_name):
+def cursor_with_rows(condition, database, table):
     r"""
     Return cursor object which can iterate through rows matching condition.
     """
     connection = sqlite3.connect(database)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM ' + table_name + ' WHERE ' + condition)
+    cursor.execute('SELECT * FROM ' + table + ' WHERE ' + condition)
     return cursor, connection
 
 
