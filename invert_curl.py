07665d869c7cb9074f6407534b49821bf1fad5d6
Jens von der Linden <jensvonderlinden@gmail.com>
2017-02-03 10:53:44 -0800

    Add cosine transform option, take real part of fft instead of abs.
    Cosine transform is better if boundary conditions are non-periodic.

--- invert_curl.py
+++ invert_curl.py
@@ -1,23 +1,31 @@
 import numpy as np
 import scipy.fftpack as fft
+import sys
+sys.path.append('../laplace_solver/')
+import laplace_solver as lsolve
 
-
-def fourier_inverse_curl(Bx, By, Bz, x, y, z):
+def fourier_inverse_curl(Bx, By, Bz, x, y, z, method='fourier'):
     r"""
     Invert curl with pseudo-spectral method described in MacKay 2006.
     """
     shape = Bx.shape
 
-    Bx_padded = np.pad(Bx, pad_width=zip(shape, shape),
-                       mode='reflect')
-    By_padded = np.pad(By, pad_width=zip(shape, shape),
-                       mode='reflect')
-    Bz_padded = np.pad(Bz, pad_width=zip(shape, shape),
-                       mode='reflect')
+    if method == 'fourier':
+        Bx_padded = np.pad(Bx, pad_width=zip(shape, shape),
+                           mode='reflect')
+        By_padded = np.pad(By, pad_width=zip(shape, shape),
+                           mode='reflect')
+        Bz_padded = np.pad(Bz, pad_width=zip(shape, shape),
+                           mode='reflect')
 
-    kx1 = np.zeros(Bx_padded[0, :, 0].size)
-    ky1 = np.zeros(By_padded[:, 0, 0].size)
-    kz1 = np.zeros(Bz_padded[0, 0, :].size)
+        kx1 = np.zeros(Bx_padded[0, :, 0].size)
+        ky1 = np.zeros(By_padded[:, 0, 0].size)
+        kz1 = np.zeros(Bz_padded[0, 0, :].size)
+
+    if method == 'cosine':
+        kx1 = np.zeros(Bx[0, :, 0].size)
+        ky1 = np.zeros(By[:, 0, 0].size)
+        kz1 = np.zeros(Bz[0, 0, :].size)
 
     dx = x[0, 1, 0] - x[0, 0, 0]
     dy = y[1, 0, 0] - y[0, 0, 0]
@@ -33,33 +41,48 @@ def fourier_inverse_curl(Bx, By, Bz, x, y, z):
 
     kx, ky, kz = np.meshgrid(kx1, ky1, kz1)
 
-    Bx_k = fft.fftn(Bx_padded)
-    By_k = fft.fftn(By_padded)
-    Bz_k = fft.fftn(Bz_padded)
+    if method == 'fourier':
+        Bx_k = fft.fftn(Bx_padded)
+        By_k = fft.fftn(By_padded)
+        Bz_k = fft.fftn(Bz_padded)
+    if method == 'cosine':
+        Bx_k = lsolve.dct_3d(shape, Bx)
+        By_k = lsolve.dct_3d(shape, By)
+        Bz_k = lsolve.dct_3d(shape, Bz)
 
     k_squared = kx**2. + ky**2. + kz**2.
 
-    Ax_k = 1j*(ky*Bz_k - kz*By_k)/k_squared
-    Ay_k = 1j*(kz*Bx_k - kx*Bz_k)/k_squared
-    Az_k = 1j*(kx*By_k - ky*Bx_k)/k_squared
+    if method == 'fourier':
+        Ax_k = 1j*(ky*Bz_k - kz*By_k)/k_squared
+        Ay_k = 1j*(kz*Bx_k - kx*Bz_k)/k_squared
+        Az_k = 1j*(kx*By_k - ky*Bx_k)/k_squared
+    if method == 'cosine':
+        Ax_k = (ky*Bz_k - kz*By_k)/k_squared
+        Ay_k = (kz*Bx_k - kx*Bz_k)/k_squared
+        Az_k = (kx*By_k - ky*Bx_k)/k_squared
 
     Ax_k[0, 0, 0] = 0.
     Ay_k[0, 0, 0] = 0.
     Az_k[0, 0, 0] = 0.
 
-    Ax_padded = np.abs(fft.ifftn(Ax_k))
-    Ay_padded = np.abs(fft.ifftn(Ay_k))
-    Az_padded = np.abs(fft.ifftn(Az_k))
+    if method == 'fourier':
+        Ax_padded = np.real(fft.ifftn(Ax_k))
+        Ay_padded = np.real(fft.ifftn(Ay_k))
+        Az_padded = np.real(fft.ifftn(Az_k))
 
-    Ax = Ax_padded[shape[0]:shape[0]*2,
-                   shape[1]:shape[1]*2,
-                   shape[2]:shape[2]*2]
-    Ay = Ay_padded[shape[0]:shape[0]*2,
-                   shape[1]:shape[1]*2,
-                   shape[2]:shape[2]*2]
-    Az = Az_padded[shape[0]:shape[0]*2,
-                   shape[1]:shape[1]*2,
-                   shape[2]:shape[2]*2]
+        Ax = Ax_padded[shape[0]:shape[0]*2,
+                       shape[1]:shape[1]*2,
+                       shape[2]:shape[2]*2]
+        Ay = Ay_padded[shape[0]:shape[0]*2,
+                       shape[1]:shape[1]*2,
+                       shape[2]:shape[2]*2]
+        Az = Az_padded[shape[0]:shape[0]*2,
+                       shape[1]:shape[1]*2,
+                       shape[2]:shape[2]*2]
+    if method == 'cosine':
+        Ax = lsolve.idct_3d(shape, Ax_k)
+        Ay = lsolve.idct_3d(shape, Ay_k)
+        Az = lsolve.idct_3d(shape, Az_k)
 
     B0_x = np.mean(Bx)
     B0_y = np.mean(By)
