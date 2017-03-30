311d98a72f10e0a36a39773d90fcf1e3876fb252
Jens von der Linden <jensvonderlinden@gmail.com>
2017-03-30 15:57:19 -0700

    Add helicity plot notebooks and script to calculate helicity after
    cutting edges of integration volume. This is not useful over then for
    sensitivity analysis.

---
+++ helicity_cut_edges.sh
@@ -0,0 +1,6 @@
+python calculate_helicity.py --to_cut 7 -7 7 -7 7 -7
+python calculate_helicity.py --to_cut 8 -8 8 -8 8 -8 
+python calculate_helicity.py --to_cut 9 -9 9 -9 9 -9
+python calculate_helicity.py --to_cut 10 -10 10 -10 10 -10 
+python calculate_helicity.py --to_cut 11 -12 12 -12 12 -12
+python calculate_helicity.py --to_cut 13 -13 13 -13 13 -13 
